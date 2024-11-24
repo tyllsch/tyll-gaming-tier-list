7a4926d0a11270b2f5ef0a98632e3db97c026868
tyllsch <tyll.schmidt@gmail.com>
2024-11-24 01:37:51 +0100

    Changed Game Descriptions

--- src/gameSetup.js
+++ src/gameSetup.js
@@ -3,7 +3,7 @@
 window.onload = function() {
     addGame(
         'Hollow Knight', 
-        'Als ich angefangen habe Hollow Knight zu spielen war die erste Emotion, die ich dabei gefühlt habe, Frustration. Mir war direkt zum Start klar, dass das Spiel quasi für mich gemacht war und ich war enorm frustriert, es nicht viel früher gespielt zu haben. Umso besser, dass es dieses Jahr so weit war<br>Hollow Knight ist simpel. Nicht im Sinne, dass es Leicht wäre, aber dass alle Mechanics super intuitiv sind und die Perfektion und Kombination die größte Herausforderung ist.<br>Es gibt kein Aspekt, den ich als Schwäche ausmachen kann. Lore, World & Boss Design, Visuals, Kampf- & Skillsystem. Alles fühlt sich enorm optimiert an. Vor allem das Map Design. Es macht anfangs einen sehr linearen Eindruck aber irgendwann steht man fast ziellos rum. Aber schnell merkt man, dass jedes Ziel mehrere Wege hat und man für "Zielloses" Erkunden belohnt wird.<br>Anders als Viele fromsoft Games gibt es zudem einen Boss Replay Mode, und jedes "Souls-Like" verdient so etwas. Ist jetzt auch das zweite Jahr in Folge, dass ein Spiel mit Boss Replay Mode mein Spiel des Jahres wird<',
+        'Als ich angefangen habe Hollow Knight zu spielen war die erste Emotion, die ich dabei gefühlt habe, Frustration. Mir war direkt zum Start klar, dass das Spiel quasi für mich gemacht war und ich war enorm frustriert, es nicht viel früher gespielt zu haben. Umso besser, dass es dieses Jahr so weit war<br>Hollow Knight ist simpel. Nicht im Sinne, dass es Leicht wäre, aber dass alle Mechanics super intuitiv sind und die Perfektion und Kombination aller Basics die größte Herausforderung ist.<br>Es gibt kein Aspekt, den ich als Schwäche ausmachen kann. Lore, World & Boss Design, Visuals, Kampf- & Skillsystem. Alles fühlt sich enorm optimiert an. Vor allem das Map Design. Es macht anfangs einen sehr linearen Eindruck aber irgendwann steht man fast ziellos rum. Aber schnell merkt man, dass jedes Ziel mehrere Wege hat und man für "Zielloses" Erkunden belohnt wird.<br>Anders als Viele fromsoft Games gibt es zudem einen Boss Replay Mode, und jedes "Souls-Like" verdient so etwas. Ist jetzt auch das zweite Jahr in Folge, dass ein Spiel mit Boss Replay Mode mein Spiel des Jahres wird (Sekiro :*)',
         'img/cover/hollowknight.jpg', 
         'img/sum/hollowknight.jpg', 
         'S'
@@ -11,7 +11,7 @@ window.onload = function() {
 	
 	addGame(
 		'Firewatch',
-		'Emotional, Visuell und von der Story ein wahnsinnig schönes Spiel. Aber mehr auch nicht.<br>Das Ende, Spoiler, hat mich ein bisschen enttäuscht und unterwältigt. Es passt wohl zum Motiv des Spiels, aber das Mysterium das Spiels war viel zu spannend für diese Konklusion.<br>Die Charaktere sind fantastisch geschrieben und vertont. Einer der besseren "Walking-Simulator" und ehrlich gesagt auch ein gutes Horror-Spiel. Zumindest fühlte es sich oft so an, ich weiß nicht, ob das so beabsichtigt war.',
+		'Emotional, Visuell und von der Story ein wahnsinnig schönes Spiel. Aber mehr auch nicht.<br>Das Ende, Spoiler, hat mich ein bisschen enttäuscht und unterwältigt. Es passt wohl zum Motiv des Spiels, aber das Mysterium des Spiels war viel zu spannend für diese Konklusion.<br>Die Charaktere sind fantastisch geschrieben und vertont. Einer der besseren "Walking-Simulator" und ehrlich gesagt auch ein gutes Horror-Spiel. Zumindest fühlte es sich oft so an, ich weiß nicht, ob das so beabsichtigt war.<br>Aber ja, das Game hatte das Problem, dass die Red Herrings, die es aufgestellt hat, spannender als die tatsächliche Story war.',
 		'img/cover/firewatch.jpg',
 		'img/sum/firewatch.jpg',
 		'C'
@@ -19,7 +19,7 @@ window.onload = function() {
 
     addGame(
         'Nine Sols', 
-        'Hab es direkt nach Hollow Knight begonnen und konnte mich nicht reinarbeiten, da es zu ähnlich war.<br>Ein Paar Monate später hab ich es lieben gelernt. Es ist offensichtlich von Sekiro und Hollow Knight inspiriert, weiß diese Inspiration aber auch umzusetzen, dass es ein spaßiges Kampfsystem ermöglicht.<br>Die Map, auch wenn anfangs unintuitiv, ist auch fantastisch gestaltet.<br>Visuell ist es wahrscheinlich das schönste Spiel was ich dieses Jahr gespielt habe. Die Farben, die handgezeichneten Paneele, alles pure Klasse<br>Die Story ist besser erzählt als Sekiro und Hollow Knight, emotional auch sehr viel ansprechender. Während ich Yi nicht immer mochte, war er trotzdem ein fantastischer Hauptcharakter. Meine Überraschung des Jahres, mein Lieblingsbossfight dieses Jahr.<br>Replayability ist leider eingeschränkt geil mit den vielen Cutscenes und keine Möglichkeit Bosse zu replayen.', 
+        'Hab es direkt nach Hollow Knight begonnen und konnte mich nicht reinarbeiten, da es zu ähnlich war und doch so anders.<br>Ein Paar Monate später hab ich es lieben gelernt. Es ist offensichtlich von Sekiro und Hollow Knight inspiriert, weiß diese Inspiration aber auch umzusetzen, dass es ein spaßiges Kampfsystem ermöglicht. <br>Die Map, auch wenn anfangs unintuitiv, ist auch fantastisch gestaltet.<br>Visuell ist es wahrscheinlich das schönste Spiel was ich dieses Jahr gespielt habe. Die Farben, die handgezeichneten Paneele, alles pure Klasse<br>Die Story ist besser erzählt als Sekiro und Hollow Knight, emotional auch sehr viel ansprechender. Während ich Yi nicht immer mochte, war er trotzdem ein fantastischer Hauptcharakter. Meine Überraschung des Jahres, mein Lieblingsbossfight dieses Jahr, insgesamt einfach geil.<br>Replayability ist leider eingeschränkt geil mit den vielen Cutscenes und keine Möglichkeit Bosse zu replayen. Da ist Hollow Knight vorraus.', 
         'img/cover/ninesols.jpg', 
         'img/sum/ninesols.jpg',
         'S'
@@ -27,7 +27,7 @@ window.onload = function() {
 
     addGame(
         'Cyberpunk Phantom Liberty', 
-        'Alles Cyberpunk bin ich total voreingenommen. Ein fantasischer DLC, der Cyberpunk Spy Thriller perfekt umsetzt. Manchmal zog es sich leicht, aber fantastisches Gameplay. Nur die Charaktere fand ich teilweise anstrengend, was mir auch leider das Ende etwas made gemacht hat, da ich nicht ganz den gewünschten emotionalen Zugang hatte.', 
+        'Alles Cyberpunk bin ich total voreingenommen. Ein fantasischer DLC, der Cyberpunk Spy Thriller perfekt umsetzt. Manchmal zog es sich leicht, aber fantastisches Gameplay. Nur die Charaktere fand ich teilweise anstrengend, was mir auch leider das Ende etwas made gemacht hat, da ich nicht ganz den gewünschten emotionalen Zugang hatte und die gewünschte Reaktion ausgeblieben ist. Ein tolles neues Kapitel aber im Cyberpunk Universum, ich werde es immer lieben.', 
         'img/cover/cp_pl.jpg', 
         'img/sum/cp_pl.jpg', 
         'A'
@@ -35,7 +35,7 @@ window.onload = function() {
 
     addGame(
         'Dredge', 
-        'Auch eine Überraschung für mich. Lovecraftian Horror ist geil und dieses Spiel vereint "Cozy-Game" und existentielle Angst perfekt.<br>Das Spiel hatte leichte Pacing Probleme, da nach ca. 50% wenig neue Mechanics hinzukommen und man nur noch die Story durchrusht aber vllt. hab ich am Anfang auch zu viel gegrindet.<br>Story war fantastisch, das Spiel hat wahrscheinlich, dicker Spoiler an der Stelle, den besten Plot Twist in einem Videospiel, den ich jemals gesehen hab.', 
+        'Auch eine Überraschung für mich. Lovecraftian Horror ist geil und dieses Spiel vereint "Cozy-Game" und existentielle Angst perfekt.<br>Das Spiel hatte leichte Pacing Probleme, da nach ca. 50% wenig neue Mechanics hinzukommen und man nur noch die Story durchgerusht wird aber vllt. hab ich am Anfang auch zu viel gegrindet.<br>Story war fantastisch, das Spiel hat wahrscheinlich, dicker Spoiler an der Stelle, den besten Plot Twist in einem Videospiel, den ich jemals gesehen hab. Ich hab die Themes und das simple Gameplay einfach gelibet', 
         'img/cover/dredge.jpg', 
         'img/sum/dredge.jpg', 
         'A'
@@ -43,7 +43,7 @@ window.onload = function() {
 
     addGame(
         'Ghost Trick: Phantom Detective', 
-        'Ich liebe Puzzlespiele mit Twists. Den Core Gameplay Loop von Ghost Trick muss man mögen, sonst hat man keinen Spaß, aber bei mir hat er auf jeden Fall gezündet.<br>Die Story und Charakter sind fantastisch geschrieben, dass man auch jenseits des normalen Bedürfnis, das Spiel durchzuspielen, ständig motiviert ist, die übergeordnete Story zu lössen.', 
+        'Ich liebe Puzzlespiele mit Twists. Den Core Gameplay Loop von Ghost Trick muss man mögen, sonst hat man keinen Spaß, aber bei mir hat er auf jeden Fall gezündet.<br>Die Story und Charakter sind fantastisch geschrieben, dass man auch jenseits des normalen Bedürfnis, das Spiel durchzuspielen, ständig motiviert ist, die übergeordnete Story zu lösen. Das Spiel ist fantastisch geschrieben, man sieht die Twists und Auflösungen eigentlich selten kommen, aber sie ergeben immer Sinn und tragen zu dem tollen Erlebnis bei', 
         'img/cover/ghosttrick.jpg', 
         'img/sum/ghosttrick.jpg', 
         'A'
@@ -51,7 +51,7 @@ window.onload = function() {
 
     addGame(
         'Signalis', 
-        'Survival Horror a la Silent Hill sind auch nicht mein Ding (Cyberpunk schon). Dafür hab ich dieses Spiel krass lieben gelernt.<br>Das Spiel wirkt super optimiert, alle Mechanics funktioneren klasse zusammen und während ich die Story auch nach ganz vielen Youtube Sessions nicht ganz verstanden hab, sind die Motive des Spiels mir überraschend nahe gekommen.', 
+        'Survival Horror a la Silent Hill sind auch nicht mein Ding (Cyberpunk aber schon, also hat das Game ne Chance von mir bekommen). Dafür hab ich dieses Spiel krass lieben gelernt.<br>Das Spiel wirkt super optimiert, alle Mechanics funktioneren klasse zusammen und während ich die Story auch nach ganz vielen Youtube Sessions nicht ganz verstanden hab, sind die Motive des Spiels mir überraschend nahe gegangen. Einsamkeit, Schicksal. Die Dynamik des Main Characters mit Ariane ist einfach nur traurig, perfekt und wunderschön.', 
         'img/cover/signalis.jpg', 
         'img/sum/signalis.jpg',
         'B'
@@ -59,7 +59,7 @@ window.onload = function() {
 
     addGame(
         "Tony Hawk's Underground", 
-        'Die Tony Hawk Reihe war ein Pfeiler meiner Kindheit. Dieser Teil ist mir aber immer entwichen, einfach weil meine Eltern mir ihn nie gekauft haben. Der Story Mode ist einer der bersten der Reihe und mal was komplett neues.<br>Als THUG2 Fan war das Gameplay nichts neues/überraschendes aber es hat einfach wahnsinnig spaß gemacht.<br>Fick Eric Sparrow.', 
+        'Die Tony Hawk Reihe war ein Pfeiler meiner Kindheit. Dieser Teil ist mir aber immer entwichen, einfach weil meine Eltern mir ihn nie gekauft haben. Der Story Mode ist einer der besten der Reihe und mal was komplett neues.<br>Als THUG2 Fan war das Gameplay nichts neues/überraschendes aber es hat einfach wahnsinnig spaß gemacht. Und das war eig die Hauptsache, mehr ist mir vom Game nicht hängen geblieben<br>Fick Eric Sparrow.', 
         'img/cover/thug.jpg', 
         'img/sum/thug.jpg', 
         'B'
@@ -67,7 +67,7 @@ window.onload = function() {
 
     addGame(
         'OlliOlli World', 
-        '', 
+        'Ich hatte seitdem ich Tony Hawk Games gespielt hab immer Probleme mich auf andere Skategames einzulassen.<br>OlliOlli World war die Ausnahme. Es ist eins dieser "Easy to learn, hard to master" Games und die Mechanics, der Flow der Levels fühlt sich einfach geil und befriedigend an. Der Artstyle, der Soundtrack. Muy Bueno. Einzige, leider große Schwäche: Nachdem Manuals eingefügt werden fühlt sich jedes Level an wie ein "Combo durchs ganze Level" und das nimmt extrem Geschwindigeit aus dem Game, da man immer restartet. Insgesamt, Pacing Probleme sind ein Issue, das Game hätte gern 20% kürzer sein können.', 
         'img/cover/olliolli.jpg', 
         'img/sum/olliolli.jpg', 
         'B'
@@ -75,7 +75,7 @@ window.onload = function() {
 
     addGame(
         'Burnout Paradise', 
-        '', 
+        'Was soll man dazu sagen. Mies satisfying das Game. Die Fahrmechanics sind intuitiv und fühlen sich gut an. Optisch und von Feedback macht das Game einiges her. Der Soundtrack ist ideal. Einfach ein Game, das man sich gerne nebenbei anmacht und zockt. Cooles Arcade Game, bin ein großer Fan von Open World Racer Games.', 
         'img/cover/burnoutparadise.jpg', 
         'img/sum/burnoutparadise.jpg', 
         'C'
@@ -83,7 +83,7 @@ window.onload = function() {
 
     addGame(
         'Duck Detective: The Secret Salami', 
-        '', 
+        'Detektivspiele sind gerade ein großes Ding für mich. Ich such immer nach idealen Games. Spiele sind leider selten so frei, dass man komplett falsch liegen kann bei einem Rätsel, aber trotzdem weiterspielt.<br>Duck Detective macht das auch nicht (die Einleitung hat vllt anderes erwarten lassen) aber ist mies charmant. Es findet ein gutes Verhältnis, dass man wirklich selbst nachdenken muss um ein Fall zu lösen, ist aber manchmal doch sehr künstlich im Prozess der Detektivarbeit. Insgesamt super charmant, aber das wars wohl.', 
         'img/cover/duckdetective.jpg', 
         'img/sum/duckdetective.jpg', 
         'C'
@@ -115,7 +115,7 @@ window.onload = function() {
 
     addGame(
         'Balatro', 
-        'Roguelite. Hätte ich das mal früher gewusst. Insgesamt besser als ein Otxo, optisch auch mein Ding.<br>Aber mehr als witzige Unterhaltung nebenbei ist das Spiel für mich auch nicht. Man merkt dem Spiel aber Qualität an und ich hatte eigentlich immer Spaß', 
+        'Roguelite. Hätte ich das mal früher gewusst. Insgesamt besser als ein Otxo, optisch auch mein Ding.<br>Aber mehr als witzige Unterhaltung nebenbei ist das Spiel für mich auch nicht. Man merkt dem Spiel aber Qualität an und ich hatte eigentlich immer Spaß. -> 20h später, find ichs echt toll. So Roguelite ist es gar nicht und die Mechanics sind super optimiert. Es macht einfach spaß neue Runs zu spielen und mit neuen Strats versuchen so weit wie möglich zu kommen. I love it', 
         'img/cover/balatro.jpg', 
         'img/sum/balatro.jpg',
         'B'
@@ -147,7 +147,7 @@ window.onload = function() {
 
     addGame(
         'Chants of Sennaar',
-        '',
+        'Wow. Ich hätte das auch in A packen können, aber dafür war leider nicht genug Content da. Es hatte ideale Puzzle/Detective Mechanics und war optisch einfach nur atemberaubend. Sprachen zu lernen damit war ein cooler Twist und ich hab die Überforderung, ganz viele Wörter zu lernen, ohne die anwenden zu können, geliebt. Es war wahrscheinlich mein Puzzle Highlights dieses Jahr, aber ich glaube ich hab an das Genre vllt. unrealisitsche Standards. Am liebsten würde ich ein Detective Game spielen wo man in den ersten 30 Minuten einen Fehler macht und das bis zum Ende des Games nicht rausbekommt :D',
         'img/cover/chantsofsennaar.jpg',
         'img/sum/chantsofsennaar.jpg',
         'B'
@@ -155,7 +155,7 @@ window.onload = function() {
 
     addGame(
         'The Dark Pictures: House of Ashes',
-        '',
+        'Ich mag das Konzept, das Game fand ich aber von der Story und den Characters bisher das schwächste Game des Entwicklers. Nur Jason war ein interessanter Charakter, die anderen waren gefühlt nur überspitzte Karikaturen. Ich hatte spaß, die Lore zu checken, aber es taten mir trotzdem zu viele Fragezeichen auf. Hat spaß gemacht, aber hängen geblieben ist wenig. Postman Alpha Out',
         'img/cover/hoa.jpg',
         'img/sum/hoa.jpg',
         'D'
@@ -163,7 +163,7 @@ window.onload = function() {
 
     addGame(
         'The Henry Stickmin Collection',
-        '',
+        'Hab nix auszusetzen an dem Game. Mechanics sind sehr basich, Quali ist wechselhaft aber es war durchaus unterhaltsam... Mehr gibts nicht zu sagen. Ich mochte die Spannbreite an mögichen Enden!',
         'img/cover/henrystickmin.jpg',
         'img/sum/henrystickmin.jpg',
         'D'
@@ -171,7 +171,7 @@ window.onload = function() {
     
     addGame(
         'Dave The Diver',
-        '',
+        'Ich plane es noch zueende zu spielen, kann gerade noch nicht viel zu sagen außer dass ichs visuell super schön finde. Ich hoffe mit der Zeit nehmen die Tutorials ab, ich hab noch das Gefühl, die machen 50% des Spiels aus. Ich erwarte fast, dass das Game auf B Tier landet.',
         'img/cover/dave.jpg',
         'img/sum/dave.jpg',
         'DNF'
@@ -179,7 +179,7 @@ window.onload = function() {
 
     addGame(
         'Dishonored 2',
-        '',
+        '2016 gestartet, Grafikkarte war nicht gut genug. 2019 gestartet, schnell Interesse verloren. 2024 gestartet, und eine geile Zeit gehabt. Die Story ist mid, aber nicht der Fokus. Das Gameplay macht verdammt spaß, das Konzept ist gewohnt geil. Das Level Design ist okay/gut aber der Feel und die Welt machen das krass wett. Ich hatte immer spaß, hätte mir gewünscht, dass mehr Rätsel drinne sind (Quasi alle Tresor Kombinationen benötigen keine Intelligenz, sondern nur Lesevermögen). Hat super spaß gemacht, die Welt und das Setting leisten aber viel Arbeit',
         'img/cover/dishonored2.jpg',
         'img/sum/dishonored2.jpg',
         'A'
@@ -187,7 +187,7 @@ window.onload = function() {
 
     addGame(
         'Citizen Sleeper',
-        '',
+        'Cyberpunk Game. Eigentlich meins, aber (und das mag jetzt dumm klingen), ich mag nicht so viel lesen :D Das Konzept, dass es ein bisschen Pen and Paper Themed ist, ist geil. Aber ich hab schnell das Interesse verloren. Vllt. spiel ich es noch zueende.',
         'img/cover/citizensleeper.jpg',
         'img/sum/citizensleeper.jpg',
         'DNF'
